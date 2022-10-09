c73f45390a42faf109c07ba59f9d96e324479d0c
shubham777580 <shubhammukherjee370@gmail.com>
2022-10-09 15:36:59 +0530

    Create Menu.tsx

---
+++ reactcapstone/react-healthcare/src/Menu.tsx
@@ -0,0 +1,19 @@
+export default function Menu(){
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">ABC Healthcare - Shubham Mukherjee</a>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link" href='/category'>Medicine Categories</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+        </nav>
+    )
+}
